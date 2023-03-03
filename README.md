6a49cf098b28776bb6bb2e6ac8aa2d818e0ef4c5
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-03 15:08:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Azure Log Analytic Workspace
+[![Changelog](https://img.shields.io/badge/changelog-release-green.svg)](CHANGELOG.md) [![Notice](https://img.shields.io/badge/notice-copyright-yellow.svg)](NOTICE) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-orange.svg)](LICENSE) [![TF Registry](https://img.shields.io/badge/terraform-registry-blue.svg)](https://registry.terraform.io/modules/)
+
+This Terraform module creates an Azure Log Analytic Workspace
+
+<!-- BEGIN_TF_DOCS -->
+
+<!-- END_TF_DOCS -->
+
+## Related documentation
+
+Microsoft Azure documentation: [Azure Log Analytic Workspace](https://learn.microsoft.com/en-us/azure/azure-monitor/logs/log-analytics-workspace-overview).
---
+++ variables.tf
@@ -0,0 +1,81 @@
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region to use."
+  type        = string
+}
+
+variable "location_short" {
+  description = "Short string for Azure location."
+  type        = string
+}
+
+variable "environment" {
+  description = "Project environment."
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name."
+  type        = string
+  validation {
+    condition     = var.stack == "" || can(regex("^[a-z0-9]([a-z0-9-]*[a-z0-9])?$", var.stack))
+    error_message = "Invalid variable: ${var.stack}. Variable name must start with a lowercase letter, end with an alphanumeric lowercase character, and contain only lowercase letters, digits, or a dash (-)."
+  }
+}
+
+variable "allow_resource_only_permissions" {
+  description = "Specifies if the log Analytics Workspace allow users accessing to data associated with resources they have permission to view, without permission to workspace. Defaults to true."
+  type        = bool
+  default     = true
+}
+
+variable "local_authentication_disabled" {
+  description = "Specifies if the log Analytics workspace should enforce authentication using Azure AD. Defaults to false."
+  type        = bool
+  default     = true
+}
+variable "sku" {
+  description = "Specifies the SKU of the Log Analytics Workspace. Possible values are Free, PerNode, Premium, Standard, Standalone, Unlimited, CapacityReservation, and PerGB2018 (new SKU as of 2018-04-03). Defaults to PerGB2018."
+  type        = string
+  default     = "PerGB2018"
+}
+
+variable "retention_in_days" {
+  description = "The workspace data retention in days. Possible values are either 7 (Free Tier only) or range between 30 and 730."
+  type        = number
+  default     = 30
+}
+
+variable "daily_quota_gb" {
+  description = "The workspace daily quota for ingestion in GB. Defaults to -1 (unlimited) if omitted."
+  type        = number
+  default     = null
+}
+
+variable "cmk_for_query_forced" {
+  description = "Is Customer Managed Storage mandatory for query management?" ### Need to reviex this part
+  type        = bool
+  default     = false
+}
+
+variable "internet_ingestion_enabled" {
+  description = "Should the Log Analytics Workspace support ingestion over the Public Internet? Defaults to true."
+  type        = bool
+  default     = false
+}
+
+variable "internet_query_enabled" { #### Need to undertsand impact
+  description = "Should the Log Analytics Workspace support querying over the Public Internet? Defaults to true."
+  type        = bool
+  default     = true
+}
+
+variable "reservation_capacity_in_gb_per_day" {
+  description = "The capacity reservation level in GB for this workspace. Must be in increments of 100 between 100 and 5000."
+  type        = number
+  default     = 100
+}
