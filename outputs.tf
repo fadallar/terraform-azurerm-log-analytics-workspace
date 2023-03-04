77a1e0411d105d8b7732c0066571c0887a69c440
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-04 05:44:33 +0100

    Adding outputs file

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "id" {
+  description = "The Log Analytics Workspace ID."
+  value       = azurerm_log_analytics_workspace.this.id
+
+}
+
+output "primary_shared_key" {
+  description = "The Primary shared key for the Log Analytics Workspace."
+  value       = azurerm_log_analytics_workspace.this.id
+
+}
+
+output "secondary_shared_key" {
+  descritption = "The Secondary shared key for the Log Analytics Workspace."
+  value        = azurerm_log_analytics_workspace.this.id
+
+}
+
+output "workspace_id" {
+  description = "The Workspace (or Customer) ID for the Log Analytics Workspace."
+  value       = azurerm_log_analytics_workspace.this.id
+
+}
